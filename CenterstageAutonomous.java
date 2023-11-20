7718aeb7f967764fc5ba42d8731f7b435bff4e1c
OysterBayFTC <149825696+OysterBayFTC@users.noreply.github.com>
2023-11-20 15:56:39 -0500

    Update CenterstageAutonomous.java

--- CenterstageAutonomous.java
+++ CenterstageAutonomous.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.Base.AutoRobotStruct;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -9,83 +11,87 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDir
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import android.graphics.Color;
+import android.view.View;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import java.util.List;
 
-@Autonomous(name = "CenterStageAutonomous")
-public class CenterstageAutonomous extends LinearOpMode {
 
-    private DcMotor motorFrontRight;
-    private DcMotor motorFrontLeft;
-    private DcMotor motorBackRight;
-    private DcMotor motorBackLeft;
-    public Servo servoClaw1;
-    public Servo servoClaw2;
-    private VisionDetector visionDetector;
+@Autonomous(name = "CenterstageAutonomous")
+public class CenterstageAutonomous extends AutoRobotStruct {
+
+    // Declare global variables
+    private boolean atLine = false;
+    private double x;
+    private double y;
+
+    // Create an instance of VisionDetector
+    VisionDetector visionDetector;
+
+    // Create color sensor object
+    private ColorSensor colorSensor;
 
     @Override
-        public void setDriverMotorPower(double FRightPower, double FLeftPower, double BRightPower, double BLeftPower) {
-        motorFrontRight.setPower(FRightPower);
-        motorFrontLeft.setPower(FLeftPower);
-        motorBackLeft.setPower(BLeftPower);
-        motorBackRight.setPower(BRightPower);
-    }
-
-    public void setDriverMotorPower(double FRightPower, double FLeftPower, double BRightPower, double BLeftPower, int s) {
-        motorFrontRight.setPower(FRightPower);
-        motorFrontLeft.setPower(FLeftPower);
-        motorBackLeft.setPower(BLeftPower);
-        motorBackRight.setPower(BRightPower);
-        sleep(s);
-
-        motorFrontRight.setPower(0);
-        motorFrontLeft.setPower(0);
-        motorBackLeft.setPower(0);
-        motorBackRight.setPower(0);
-        sleep(100);
-    }  public void setClawPos(double pos1, double pos2) {
-        servoClaw1.setPosition(pos1);
-        servoClaw2.setPosition(pos2);
-    }     public void translateRight(double m) {
-        motorFrontRight.setPower(-m);
-        motorFrontLeft.setPower(m);
-        motorBackLeft.setPower(-m);
-        motorBackRight.setPower(m);
-    }
-
-    public void translateLeft(double m) {
-        motorFrontRight.setPower(m);
-        motorFrontLeft.setPower(-m);
-        motorBackLeft.setPower(m);
-        motorBackRight.setPower(-m);
-        
-    }
     public void runOpMode() {
-        // Create an instance of VisionDetector and pass 'this' as an argument
         visionDetector = new VisionDetector(this);
-        telemetry.addData(">", "Touch Play to start OpMode");
-        telemetry.update();
+
+        // Declare motors and servos
+        colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
+
+        // Start TensorFlow Object Detection
+        visionDetector.startDetection();
+
         waitForStart();
 
-        if (opModeIsActive()) {
-            visionDetector.startDetection();
+        while (opModeIsActive()) {
 
-            while (opModeIsActive()) {
-                visionDetector.telemetryTfod(this);
+
+            // Read color values
+            float[] hsvValues = readColor(colorSensor);
+            List<Recognition> recognitions = visionDetector.getRecognitions();
+            visionDetector.telemetryTfod(this);
+            telemetry.update();
+            sleep(20);
+
+            if (recognitions.size() > 0) {
+                Recognition recognition = recognitions.get(0);
+                x = (recognition.getLeft() + recognition.getRight()) / 2;
+                y = (recognition.getTop() + recognition.getBottom()) / 2;
+
+            } 
+
+            // Check if the robot is on the line
+            if (hsvValues[0] < 25) {
+                atLine = true;
+                telemetry.addData("On", "the line!");
                 telemetry.update();
-                sleep(20);
-                Recognition recognition = visionDetector.getRecognitions().size() > 0 ?
-                        visionDetector.getRecognitions().get(0) : null;
 
-                // Check if recognition exists and confidence is greater than 0.90
-                if (recognition != null && recognition.getConfidence() > 0.9) {
-                    // Perform your action here
-                    telemetry.addData("Action", "Confidence is high! Perform your action here.");
-                    telemetry.update();
-
+                // Perform actions upon reaching the line
+                // (e.g., lift servo, initiate other movements)
+            } else {
+                atLine = false;
             }
 
-            visionDetector.stopDetection();
+            // Update telemetry data
+            telemetry.addData("At Line: ", atLine);
+            telemetry.addData("X: ", x);
+            telemetry.addData("Y: ", y);
+            telemetry.update();
         }
-        
     }
+
+    public float[] readColor(ColorSensor colorSensor) {
+        float hsvValues[] = {0F, 0F, 0F};
+        final float values[] = hsvValues;
+
+        boolean bPrevState = false;
+        boolean bCurrState = false;
+
+        boolean bLedOn = true;
+
+        bPrevState = bCurrState;
+        Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
+
+        return hsvValues;
     }
 }
