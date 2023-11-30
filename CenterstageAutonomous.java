38ad3da8d0db1e709270fadf61ff27e829ca44c3
OysterBayFTC <149825696+OysterBayFTC@users.noreply.github.com>
2023-11-30 11:08:37 -0500

    Update CenterstageAutonomous.java

--- CenterstageAutonomous.java
+++ CenterstageAutonomous.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.teamcode.VisionDetectorOne;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 import android.graphics.Color;
 import android.view.View;
@@ -25,14 +26,14 @@ public class CenterstageAutonomous extends AutoRobotStruct {
     private double y;
 
     // Create an instance of VisionDetector
-    VisionDetector visionDetector;
+    VisionDetectorOne visionDetector;
 
     // Create color sensor object
     private ColorSensor colorSensor;
 
     @Override
     public void runOpMode() {
-        visionDetector = new VisionDetector(this);
+        visionDetector = new VisionDetectorOne(this);
 
         // Declare motors and servos
         colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
@@ -57,17 +58,10 @@ public class CenterstageAutonomous extends AutoRobotStruct {
                 x = (recognition.getLeft() + recognition.getRight()) / 2;
                 y = (recognition.getTop() + recognition.getBottom()) / 2;
 
-            } 
-
+            } else {
+                
+            }
             // Check if the robot is on the line
-            if (atLine = false) {
-                visionDetector.stopDetection():
-                while (atLine = false) {
-                    setDriverMotorPower(0.3, 0.3, 0.3, 0.3);
-                        if (atLine = true) {
-                            setDriverMotorPower(0.0, 0.0, 0.0, 0.0);
-                        }
-                    }
 
             // Update telemetry data
             telemetry.addData("At Line: ", atLine);
