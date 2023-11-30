8f74d430b50e541ecbb9938c893b33db48189760
OysterBayFTC <149825696+OysterBayFTC@users.noreply.github.com>
2023-11-30 15:30:45 -0500

    Update VisionDetector.java

--- Base/VisionDetector.java
+++ Base/VisionDetector.java
@@ -10,15 +10,13 @@ import org.firstinspires.ftc.teamcode.Base.AutoRobotStruct;
 
 import java.util.List;
 
-public class VisionDetector extends AutoRobotStruct {
-    private static final boolean USE_WEBCAM = true;
-    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/blue1.tflite";
-    private static final String[] LABELS = {"Team Element"};
-    public int position = 0;
+public class VisionDetectorOne extends AutoRobotStruct {
 
-    public TfodProcessor tfod;
-    public VisionPortal visionPortal;
-    public VisionDetector(LinearOpMode opMode) {
+    private static final boolean USE_WEBCAM = true;
+    private TfodProcessor tfod;
+    private VisionPortal visionPortal;
+
+    public VisionDetectorOne(LinearOpMode opMode) {
         initTfod(opMode);
     }
 
@@ -47,44 +45,21 @@ public class VisionDetector extends AutoRobotStruct {
     public List<Recognition> getRecognitions() {
         return tfod.getRecognitions();
     }
+
     public List<Recognition> getObjects() {
-    return tfod.getRecognitions();
-}
+        return tfod.getRecognitions();
+    }
 
     public void telemetryTfod(LinearOpMode opMode) {
         List<Recognition> currentRecognitions = getRecognitions();
         opMode.telemetry.addData("# Objects Detected", currentRecognitions.size());
-// setDriverMotorPower(double FRightPower, double FLeftPower, double BRightPower, double BLeftPower, int s)
+
         for (Recognition recognition : currentRecognitions) {
             double x = (recognition.getLeft() + recognition.getRight()) / 2;
             double y = (recognition.getTop() + recognition.getBottom()) / 2;
-            if (x > -10 && x < 290) {
-                    opMode.telemetry.addData("Left position", ".");
-                    opMode.telemetry.addData("Line = ", atLine);
-                    position = 1;
-                    break;
-            } else if (x > 300 && x < 575) {
-                    opMode.telemetry.addData("Middle position", ".");
-                    opMode.telemetry.addData("Line = ", atLine);
-                    position = 2;
-                    break;
-            } else {
-                    opMode.telemetry.addData("Right position", ".");
-                    opMode.telemetry.addData("Line = ", atLine);
-                    position = 3;
-                    break;
-            } if (atLine = false) {
-                while (atLine = false) {
-                    setDriverMotorPower(0.3, 0.3, 0.3, 0.3);
-                        if (atLine = true) {
-                            setDriverMotorPower(0.0, 0.0, 0.0, 0.0);
-                        }
-                    }
-
-            opMode.telemetry.addData("", " ");
-            opMode.telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
-            opMode.telemetry.addData("- Position", "%.0f / %.0f", x, y);
-            opMode.telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+            opMode.telemetry.addData("Object Position (X, Y)", "%.2f, %.2f", x, y);
         }
+
+        opMode.telemetry.update();
     }
 }
