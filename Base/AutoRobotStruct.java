24607f152fea1dc99b442f91b179f8439c5cebe6
OysterBayFTC <149825696+OysterBayFTC@users.noreply.github.com>
2023-12-08 17:04:37 -0500

    Update AutoRobotStruct.java

--- Base/AutoRobotStruct.java
+++ Base/AutoRobotStruct.java
@@ -1,56 +1,57 @@
+
 package org.firstinspires.ftc.teamcode.Base;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import android.graphics.Color;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx; // Use DcMotorEx for encoder support
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.TouchSensor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-
 public class AutoRobotStruct extends LinearOpMode {
-    DcMotor motorFrontRight;
-    DcMotor motorFrontLeft;
-    DcMotor motorBackRight;
-    DcMotor motorBackLeft;
-    public Servo servoClaw1;
-    public Servo servoClaw2;
+    public DcMotorEx motorFrontRight;
+    public DcMotorEx motorFrontLeft;
+    public DcMotorEx motorBackRight;
+    public DcMotorEx motorBackLeft;
+    Servo servoClaw1;
+    Servo servoClaw2;
     public ColorSensor colorSensor;
     public boolean bLedOn = true;
     public boolean atLine = false;
     public String estColor;
     public int position = 0;
-
+    public TouchSensor leftSensor;
+    public TouchSensor rightSensor;
+    public Servo dragBlock;
 
     @Override
-    public void runOpMode() throws InterruptedException { }
+    public void runOpMode() throws InterruptedException {}
 
     public void initRunner() throws InternalError {
-        motorFrontRight = hardwareMap.get(DcMotor.class, "motorFrontRight"); // 2
-        motorFrontLeft = hardwareMap.get(DcMotor.class, "motorFrontLeft"); // 3
-        motorBackLeft = hardwareMap.get(DcMotor.class, "motorBackLeft"); // 1
-        motorBackRight = hardwareMap.get(DcMotor.class, "motorBackRight"); // 0
-        servoClaw1 = hardwareMap.get(Servo.class, "servoClaw1");
-        servoClaw2 = hardwareMap.get(Servo.class, "servoClaw2");
+        motorFrontRight = hardwareMap.get(DcMotorEx.class, "motorFrontRight");
+        motorFrontLeft = hardwareMap.get(DcMotorEx.class, "motorFrontLeft");
+        motorBackLeft = hardwareMap.get(DcMotorEx.class, "motorBackLeft");
+        motorBackRight = hardwareMap.get(DcMotorEx.class, "motorBackRight");
 
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        leftSensor = hardwareMap.touchSensor.get("leftSensor");
+        rightSensor = hardwareMap.touchSensor.get("rightSensor");
+        dragBlock = hardwareMap.get(Servo.class, "dragBlock");
+        dragBlock.setPosition(0.05);
 
-        // reverse direction for this drive train
         motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
         motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
     }
-
-
-    //telemetry.addData("Distance (cm)", "%.3f", ((DistanceSensor) colorSensor).getDistance(DistanceUnit.CM));
-
-
 
     public void setDriverMotorPower(double FRightPower, double FLeftPower, double BRightPower, double BLeftPower) {
         motorFrontRight.setPower(FRightPower);
@@ -58,16 +59,29 @@ public class AutoRobotStruct extends LinearOpMode {
         motorBackLeft.setPower(BLeftPower);
         motorBackRight.setPower(BRightPower);
     }
-     public void setDriverMotorZero() {
+
+    public void setDriverMotorPower(double FRightPower, double FLeftPower, double BRightPower, double BLeftPower, int s) {
+        motorFrontRight.setPower(FRightPower);
+        motorFrontLeft.setPower(FLeftPower);
+        motorBackLeft.setPower(BLeftPower);
+        motorBackRight.setPower(BRightPower);
+        sleep(s);
+    }
+
+    public void setDriverMotorZero() {
         motorFrontRight.setPower(0);
         motorFrontLeft.setPower(0);
         motorBackLeft.setPower(0);
         motorBackRight.setPower(0);
         sleep(100);
-    }  public void setClawPos(double pos1, double pos2) {
+    }
+
+    public void setClawPos(double pos1, double pos2) {
         servoClaw1.setPosition(pos1);
         servoClaw2.setPosition(pos2);
-    }   public void translateRight(double m) {
+    }
+
+    public void translateRight(double m) {
         motorFrontRight.setPower(-m);
         motorFrontLeft.setPower(m);
         motorBackLeft.setPower(-m);
@@ -79,50 +93,5 @@ public class AutoRobotStruct extends LinearOpMode {
         motorFrontLeft.setPower(-m);
         motorBackLeft.setPower(m);
         motorBackRight.setPower(-m);
-        
-    } public void moveToLine() {
-        while (!atLine) {
-        setDriverMotorPower(0.25, 0.25, 0.25, 0.25);
-    }
-    setDriverMotorPower(0, 0, 0, 0);
-    sleep(750);
-}
-
-    
-    
-    
-          public float[] readColor(ColorSensor colorSensor) {
-        float hsvValues[] = {0F,0F,0F};
-        final float values[] = hsvValues;
-
-        boolean bPrevState = false;
-        boolean bCurrState = false;
-
-        boolean bLedOn = true;
-        
-        bPrevState = bCurrState;
-        Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
-
-        telemetry.addData("LED", bLedOn ? "On" : "Off");
-        telemetry.addData("Clear", colorSensor.alpha());
-        telemetry.addData("Red  ", colorSensor.red());
-        telemetry.addData("Green", colorSensor.green());
-        telemetry.addData("Blue ", colorSensor.blue());
-        telemetry.addData("Hue", hsvValues[0]);
-        telemetry.addData("Saturation", "%.3f", hsvValues[1]);
-        telemetry.update();
-        
-        if (hsvValues[0] < 25) {
-                atLine = true;
-                telemetry.addData("On", "the line!");
-                telemetry.update();
-
-                // Perform actions upon reaching the line
-                // (e.g., lift servo, initiate other movements)
-            } else {
-                atLine = false;
-            }
-        // int[] colorValues = {colorSensor.red(), colorSensor.green(), colorSensor.blue()};
-        return hsvValues;
     }
 }
