cd62bcedbae5377c76b79c4ff62a9b4fdec744e4
QuinnRainey314 <90530493+QuinnRainey314@users.noreply.github.com>
2023-12-09 19:52:29 -0500

    Update CenterstageAutonomous.java

--- CenterstageAutonomous.java
+++ CenterstageAutonomous.java
@@ -1,4 +1,4 @@
-
+package org.firstinspires.ftc.teamcode;
 
 import org.firstinspires.ftc.teamcode.Base.AutoRobotStruct;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -18,52 +18,31 @@ import java.util.List;
 
 @Autonomous(name = "CenterstageAutonomous")
 public class CenterstageAutonomous extends AutoRobotStruct {
+
     private double x;
     private double y;
-    private BNO055IMU imu;
     private VisionDetectorOne visionDetector;
     private long startTime;
     private boolean positionSet = false;
+    private BNO055IMU imu;  // Use BNO055IMU for BHI260AP
+    private Orientation lastAngles = new Orientation();
 
-    private void initializeIMU() {
-        try {
-            // Initialize IMU
-            BNO055IMU.Parameters imuParameters = new BNO055IMU.Parameters();
-            imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-            imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-            imuParameters.loggingEnabled = true;
-            imuParameters.loggingTag = "IMU";
-            imu = hardwareMap.get(BNO055IMU.class, "imu");
-
-            if (imu.isGyroCalibrated()) {
-                telemetry.addData("IMU", "Calibration Complete");
-            } else {
-                telemetry.addData("IMU", "Calibration Failed");
-            }
-        } catch (Exception e) {
-            telemetry.addData("IMU Initialization Error", e.getMessage());
-            imu = null;
-        }
-        telemetry.update();
-    }
-
+    @Override
     public void runOpMode() {
-        initializeIMU();
-        initRunner();
         visionDetector = new VisionDetectorOne(this);
         colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
 
-        // ... (existing code)
+        // Rest of your code...
 
-        waitForStart();
-
-        startTime = System.currentTimeMillis();
         while (opModeIsActive() && !positionSet) {
-            if (System.currentTimeMillis() - startTime < 5000) {
-                List<Recognition> recognitions = visionDetector.getRecognitions();
-                visionDetector.telemetryTfod(this);
-                telemetry.update();
-                sleep(20);
+            List<Recognition> recognitions = visionDetector.getRecognitions();
+            visionDetector.telemetryTfod(this);
+            telemetry.update();
+            sleep(20);
+
+            // Check if a model is detected within 5 seconds
+            if (System.currentTimeMillis() - startTime < 5000 && !positionSet) {
                 if (recognitions.size() > 0) {
                     Recognition recognition = recognitions.get(0);
                     x = (recognition.getLeft() + recognition.getRight()) / 2;
@@ -74,123 +53,108 @@ public class CenterstageAutonomous extends AutoRobotStruct {
                         telemetry.addData("Left position", ".");
                         telemetry.addData("Line = ", atLine);
                         position = 1;
-                        visionDetector.stopDetection();
+                        positionSet = true;
                     } else if (x > 300 && x < 575) {
                         telemetry.addData("Middle position", ".");
                         telemetry.addData("Line = ", atLine);
                         position = 2;
-                        visionDetector.stopDetection();
+                        positionSet = true;
                     } else {
                         telemetry.addData("Right position", ".");
                         telemetry.addData("Line = ", atLine);
                         position = 3;
-                        visionDetector.stopDetection();
-                    }
-
-                    // Additional logic based on the detected position after reaching the line
-                    if (!atLine && position > 0) {
-                        moveTowardLine();
-
-                        if (position == 1) {
-                            turnLeft();
-                            dragBlock.setPosition(0.5);
-                            setDriverMotorPower(0.15, 0.15, 0.15, 0.15, 100);
-                        } else if (position == 2) {
-                            setDriverMotorPower(-0.15, -0.15, -0.15, -0.15, 100);
-                            dragBlock.setPosition(0.5);
-                        } else if (position == 3) {
-                            turnRight();
-                            dragBlock.setPosition(0.5);
-                            setDriverMotorPower(0.15, 0.15, 0.15, 0.15, 100);
-                        }
+                        positionSet = true;
                     }
                 }
+            } else if (!positionSet) {
+                // Assume position 3 after 5 seconds
+                position = 3;
+                positionSet = true;
+            }
+
+            // Move to the line if position is valid
+            if (position > 0) {
+                moveTowardLine();
+            }
+        }
+
+        // Additional logic based on the detected position after reaching the line
+        if (atLine && position > 0) {
+            if (position == 1) {
+                setDriverMotorPower(0.15, 0.15, 0.15, 0.15, 150);
+                sleep(500);
+                turnLeft(90); // Turn left 90 degrees using gyro
+                moveTowardLine();
+                dragBlock.setPosition(0.5);
+                sleep(200);
+            } else if (position == 2) {
+                // Move forward for position 2
+                setDriverMotorPower(0.15, 0.15, 0.15, 0.15, 150);
+                sleep(500);
+                dragBlock.setPosition(0.5);
+                sleep(200);
+                setDriverMotorPower(0.15, 0.15, 0.15, 0.15, 400);
+                setDriverMotorPower(0.11, 0.11, 0.11, 0.11, 3000);
+            } else if (position == 3) {
+                // Turn right for position 3
+                dragBlock.setPosition(0.5);
+                // Use the gyro to turn right
+                turnRight(90);
             }
         }
     }
 
-    private void turnLeft() {
-        setDriverMotorPower(0.20, -0.20, 0.20, -0.20, 1500);
-        telemetry.addData("Turning Left", "");
-        telemetry.update();
+    private void turnLeft(double targetAngle) {
+        double initialAngle = getHeading();
+        double currentAngle = initialAngle;
+
+        while (opModeIsActive() && Math.abs(currentAngle - initialAngle) < targetAngle) {
+            setDriverMotorPower(0.15, -0.15, 0.15, -0.15);
+            telemetry.addData("Turning Left", "Current Angle: %.2f", currentAngle);
+            telemetry.update();
+
+            currentAngle = getHeading();
+        }
+
+        setDriverMotorZero();
     }
 
-    private void turnRight() {
-        setDriverMotorPower(-0.20, 0.20, -0.20, 0.20, 1500);
-        telemetry.addData("Turning Right", "");
-        telemetry.update();
+    private void turnRight(double targetAngle) {
+        double initialAngle = getHeading();
+        double currentAngle = initialAngle;
+
+        while (opModeIsActive() && Math.abs(currentAngle - initialAngle) < targetAngle) {
+            setDriverMotorPower(-0.15, 0.15, -0.15, 0.15);
+            telemetry.addData("Turning Right", "Current Angle: %.2f", currentAngle);
+            telemetry.update();
+
+            currentAngle = getHeading();
+        }
+
+        setDriverMotorZero();
+    }
+
+    private double getHeading() {
+        Orientation angles = imu.getAngularOrientation(AngleUnit.DEGREES);
+        return angles.firstAngle;
     }
 
     private void moveTowardLine() {
-    long startTime = System.currentTimeMillis();
-
-    // Initialize encoder targets
-    int targetPosition = 2000; // Adjust this value based on your encoder counts
-
-    while (!atLine && position > 0 && opModeIsActive()) {
-        // Move using encoders
-        motorFrontLeft.setTargetPosition(targetPosition);
-        motorFrontLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        motorFrontLeft.setPower(0.5);
-
-        motorFrontRight.setTargetPosition(targetPosition);
-        motorFrontRight.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        motorFrontRight.setPower(0.5);
-
-        motorBackLeft.setTargetPosition(targetPosition);
-        motorBackLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        motorBackLeft.setPower(0.5);
-
-        motorFrontRight.setTargetPosition(targetPosition);
-        motorFrontRight.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        motorFrontRight.setPower(0.5);
-
-        while (motorFrontRight.isBusy()) {
-            telemetry.addData("Moving to Position", "...");
-            telemetry.update();
-        }
-
-        // Rest of your code...
-    }
-
-    // Reset encoders after reaching the line
-    setDriverMotorZero();
-
-    telemetry.addData("Moving to ", "LINE");
-    telemetry.addData("Elapsed Time", "%d ms", System.currentTimeMillis() - startTime);
-    telemetry.update();
-
-    // Check for the line detection condition
-    float[] hsvValues = readColor(colorSensor);
-    telemetry.addData("Hue Value", "%.2f", hsvValues[0]);
-
-    if (hsvValues[0] < 25) {
-        atLine = true;
-        setDriverMotorZero();
-        telemetry.addData("Line Detected", "Exiting the loop");
-        return;
-    } else {
-        atLine = false;
-    }
-
-    sleep(100);
-
-    if (System.currentTimeMillis() - startTime > 5000) {
-        telemetry.addData("Timeout", "Line detection took too long.");
-        positionSet = true;
-        return;
-    }
-}
-
-    private void moveToTouch() {
-        while (!leftSensor.isPressed() || !rightSensor.isPressed()) {
+        while (!atLine && position > 0) {
             setDriverMotorPower(-0.11, -0.11, -0.11, -0.11);
-            if (leftSensor.isPressed() || rightSensor.isPressed()) {
+            telemetry.addData("Moving to ", "LINE");
+            telemetry.update();
+
+            // Check for the line detection condition
+            float[] hsvValues = readColor(colorSensor);
+            if (hsvValues[0] < 25) {
+                atLine = true;
                 setDriverMotorZero();
-                break;  // Add this line to exit the loop
-            }
+                return; // Exit the loop after reaching the line
+            }  else {
+            atLine = false;
         }
-    }
+
 
     public float[] readColor(ColorSensor colorSensor) {
         float hsvValues[] = {0F, 0F, 0F};
